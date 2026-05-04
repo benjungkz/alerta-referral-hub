4342377adb86fc55291d77050cc699468b6a01c7
Ben Jung <benjungkz@gmail.com>
2026-05-04 14:01:26 -0400

    feat(shopify): add order webhook handler and apply referral tags to orders

---
+++ lib/ShopifyClient.ts
@@ -0,0 +1,15 @@
+import { GraphQLClient } from "graphql-request";
+
+const SHOPIFY_STORE_DOMAIN = process.env.SHOPIFY_STORE_DOMAIN!;
+const SHOPIFY_ADMIN_ACCESS_TOKEN = process.env.SHOPIFY_ADMIN_ACCESS_TOKEN!;
+const SHOPIFY_API_VERSION = process.env.SHOPIFY_API_VERSION || "2026-01";
+
+export const shopifyClient = new GraphQLClient(
+  `https://${SHOPIFY_STORE_DOMAIN}/admin/api/${SHOPIFY_API_VERSION}/graphql.json`,
+  {
+    headers: {
+      "X-Shopify-Access-Token": SHOPIFY_ADMIN_ACCESS_TOKEN,
+      "Content-Type": "application/json",
+    },
+  },
+);
